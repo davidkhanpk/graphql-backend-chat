ed0b483fb57027ac083c1597607d89909158a662
davidkhan <daoud@clarybusinessmachines.com>
2020-09-26 01:20:16 +0500

    fix port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const contextMiddleware = require('./utils/contextMiddleware')
 
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 
 
 const server = new ApolloServer({
