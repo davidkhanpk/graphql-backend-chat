fd3a515e9ad0d291c95b91217a78043197d6473b
davidkhan <daoud@clarybusinessmachines.com>
2020-09-26 23:22:45 +0500

    deploy

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { sequelize } = require('./models')
+const { ApolloServer, gql } = require("apollo-server");
+const { sequelize } = require("./models");
 // require('dotenv').config()
 
 // The GraphQL schema
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const contextMiddleware = require('./utils/contextMiddleware')
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const contextMiddleware = require("./utils/contextMiddleware");
 
-const PORT = process.env.PORT || 4000  
-
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: contextMiddleware,
-  subscriptions: { path: '/'}
+  subscriptions: { path: "/" },
 });
 
-server.listen({port: PORT}).then(({ url, subUrl }) => {
+server.listen({ port: PORT }).then(({ url, subUrl }) => {
   console.log(`🚀 Server ready at ${url}`);
   console.log(`🚀 Subscription ready at ${subUrl}`);
-  sequelize.authenticate().then(() => {
+  sequelize
+    .authenticate()
+    .then(() => {
       console.log("Database Connected");
-  }).catch()
+    })
+    .catch();
 });
